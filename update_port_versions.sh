8bad1c91e1bf33087a3c5b9debf53e319d649928
Israel Chauca Fuentes <israel@chauca.net>
2013-07-02 20:48:59 -0400

    Update scripts.

--- update_port_versions.sh
+++ update_port_versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update Vim's Portfile
-VERSION=$(curl https://raw.github.com/b4winckler/macvim/vim/.hgtags 2>/dev/null | tail -n 1 | sed -e 's/^[0-9a-f]*  *v//;s/-/./g;')
+VERSION=$(curl https://vim.googlecode.com/hg/.hgtags 2>/dev/null | tail -n 1 | sed -e 's/^[0-9a-f]*  *v//;s/-/./g;')
 if [[ $VERSION =~ ^7\.[3-9]\.[0-9]+$ ]]; then
   sed -i -e "s/^\(version  *\)[^ ]*\$/\1$VERSION/" /users/israel/ports/editors/vim/Portfile
   echo "Vim's version updated to \"$VERSION\"."
@@ -14,7 +14,7 @@ unset VERSION
 VERSION=$(curl https://raw.github.com/b4winckler/macvim/master/.hgtags 2>/dev/null | tail -n 1 | sed -e 's/^[0-9a-f]*  *v//;s/-/./g;')
 if [[ $VERSION =~ ^7\.[3-9]\.[0-9]+$ ]]; then
   sed -i -e "s/^\(version  *\)[^ ]*\$/\1$VERSION/" /users/israel/ports/editors/MacVim/Portfile
-  echo "Vim's version updated to \"$VERSION\"."
+  echo "MacVim's version updated to \"$VERSION\"."
 else
   echo "There was a problem getting the current MacVim's version: \"$VERSION\"."
 fi
