d5851e2c87d138f76c4cb9d1785fe4c9516dac81
Israel Chauca Fuentes <israel@chauca.net>
2015-06-18 18:22:57 -0400

    Use commit hash instead of branch name in vim-dev's Portfile.

--- update_port_versions.sh
+++ update_port_versions.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
 # Update Vim's Portfile
-VIMVERSION=7.4
+VIMVERSION=$(curl https://raw.githubusercontent.com/vim/vim/master/src/version.h 2>/dev/null | sed -n '/^#define VIM_VERSION_SHORT/{s/^#define VIM_VERSION_SHORT[[:blank:]]*"\([0-9.ab]*\)"/\1/;p;q;}')
 PATCHLEVEL=$(curl https://raw.githubusercontent.com/vim/vim/master/src/version.c 2>/dev/null | sed -n '/[0-9],$/{s/,//;s/[ \t]*//;p;q;}')
 VERSION=$VIMVERSION.$PATCHLEVEL
+PORTFILE=/Users/israel/ports/editors/vim/Portfile
+SHA=$(curl https://api.github.com/repos/vim/vim/commits/master 2>/dev/null | sed -n '/sha/{s/ *"sha": *"\([0-9a-f]*\)",/\1/;p;q;}')
 if [[ $VERSION =~ ^7\.[3-9a-zA-Z]+ ]]; then
-  sed -i -e "s/^\(version  *\)[^ ]*\$/\1$VERSION/" /users/israel/ports/editors/vim/Portfile
-  echo "Vim's version updated to \"$VERSION\"."
+  if sed -i -e "s/^\(version  *\)[^ ]*\$/\1$VERSION/" $PORTFILE; then
+    echo "Vim's version updated to \"$VERSION\"."
+  else
+    echo "Could not update Vim's version." &>2
+  fi
+  if sed -i -e "/^set gitcommit/s/[0-9a-z]*\$/$SHA/" $PORTFILE; then
+    echo "gitcommit updated to $SHA"
+  else
+    echo "Could not update gitcommit." &>2
+  fi
 else
-  echo "There was a problem getting the current Vim's version: \"$VERSION\"."
+  echo "There was a problem getting the current Vim's version: \"$VERSION\"." &>2
 fi
 
 # Update MacVim's Portfile
@@ -27,5 +37,5 @@ if [[ $VERSION =~ ^7\.[3-9a-zA-Z]+ ]]; then
   sed -i -e "s/^\(version  *\)[^ ]*\$/\1$VERSION/" /users/israel/ports/editors/MacVim/Portfile
   echo "MacVim's version updated to \"$VERSION\"."
 else
-  echo "There was a problem getting the current MacVim's version: \"$VERSION\"."
+  echo "There was a problem getting the current MacVim's version: \"$VERSION\"." &>2
 fi
