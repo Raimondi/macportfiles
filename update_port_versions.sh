20907cc16d08504f8e79d06ab8ed95d1e233d049
Israel Chauca Fuentes <israel@chauca.net>
2015-06-18 14:23:40 -0400

    Update vim-dev.

--- update_port_versions.sh
+++ update_port_versions.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Update Vim's Portfile
-VERSION=$(curl https://vim.googlecode.com/hg/.hgtags 2>/dev/null | tail -n 1 | sed -e 's/^[0-9a-f]*  *v//;s/-/./g;')
+VIMVERSION=7.4
+PATCHLEVEL=$(curl https://raw.githubusercontent.com/vim/vim/master/src/version.c 2>/dev/null | sed -n '/[0-9],$/{s/,//;s/[ \t]*//;p;q;}')
+VERSION=$VIMVERSION.$PATCHLEVEL
 if [[ $VERSION =~ ^7\.[3-9a-zA-Z]+ ]]; then
   sed -i -e "s/^\(version  *\)[^ ]*\$/\1$VERSION/" /users/israel/ports/editors/vim/Portfile
   echo "Vim's version updated to \"$VERSION\"."
@@ -10,8 +12,17 @@ else
 fi
 
 # Update MacVim's Portfile
+unset VIMVERSION
+unset PATCHLEVEL
 unset VERSION
-VERSION=7.4.$(curl https://raw.githubusercontent.com/qvacua/macvim/master/src/version.c 2>/dev/null | sed -n -e '/[0-9],$/{s/,//;s/[ \t]*//;p;}' | head -n 1)
+
+VIMVERSION=7.4
+PATCHLEVEL=$(curl https://raw.githubusercontent.com/macvim-dev/macvim/master/src/version.c 2>/dev/null | sed -n '/[0-9],$/{s/,//;s/[ \t]*//;p;q;}')
+COMMITDATE=$(curl https://github.com/macvim-dev/macvim/commits/master 2>/dev/null \
+  | sed -n '/Commits on/{s/Jan/01/;s/Feb/02/;s/Mar/03/;s/Apr/04/;s/May/05/;s/Jun/06/;s/Jul/07/;s/Aug/08/;s/Sep/09/;s/Oct/10/;s/Nov/11/;s/Dec/12/;p;q;}' \
+  | sed 's/\([0-9] \)\([1-9],\)/\10\2/' \
+  | sed 's/.* \([0-9][0-9]\) \([0-3][0-9]\), \([0-9]\{4\}\)/\3\1\2/')
+VERSION=$VIMVERSION.$PATCHLEVEL.$COMMITDATE
 if [[ $VERSION =~ ^7\.[3-9a-zA-Z]+ ]]; then
   sed -i -e "s/^\(version  *\)[^ ]*\$/\1$VERSION/" /users/israel/ports/editors/MacVim/Portfile
   echo "MacVim's version updated to \"$VERSION\"."
